d4ef482e4404b2539396bc2f25993c5c1ae7c0b3
Stephan Hoyer <stephan@noname.(none)>
2009-02-22 22:06:16 +0100

    adding templates and views for frontend

--- movie/views.py
+++ movie/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from models import Movie, Person
+from django.db import connection
+
+
+def get_detailed_movies(request, order_by, limit=10):
+    return render_to_response("detailed_movies.html", {"movies" : Movie.objects.order_by(order_by)[:limit], })
+
+def get_movie(request, id):
+    movie = Movie.objects.select_related().get(id=id)
+    return render_to_response("movie.html", locals())
+
+def get_movies(request, limit=10, order_by="-created"):
+    return render_to_response("movies.html", {"movies" : Movie.objects.order_by(order_by)[:limit]})
+
+def get_persons(request, limit=10, order_by="-created"):
+    return render_to_response("persons.html", {"persons" : Person.objects.order_by(order_by)[:limit]})
+
+def get_person(request, id):
+    return render_to_response("person.html", {"person" : Person.objects.get(id=id)})
--- urls.py
+++ urls.py
@@ -2,6 +2,8 @@ from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from movie.views import *
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -14,4 +16,15 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/(.*)', admin.site.root),
+
+    (r'^movie/(?P<id>\d+)$', get_movie),
+    (r'^movies/detailed$', get_detailed_movies, {'order_by': 'title'}),
+    (r'^$', get_detailed_movies, {'limit': 10, 'order_by':'-created'}),
+    (r'^movies/detailed/orderby/(?P<order_by>.*?)/limit/(?P<limit>.*?)/$', get_detailed_movies),
+    (r'^movies/detailed/orderby/(?P<order_by>.*?)/$', get_detailed_movies),
+    (r'^movies$', get_movies, {'order_by': 'title'}),
+    (r'^movies/orderby/(?P<order_by>.*)/$', get_movies),
+    
+    (r'^persons/orderby/(?P<order_by>.*)/$', get_persons),
+    (r'^person/(?P<id>\d+)$', get_person),
 )
