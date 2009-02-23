02f014e09fafe71d30e5aeb41c32327e911d211d
streetcleaner <mail@streetcleaner.org>
2009-02-23 21:34:47 +0100

    Splitted README and moved settings.py to settings.dev.py.

--- settings.dev.py
+++ settings.dev.py
@@ -11,7 +11,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME =  path.realpath(path.curdir) + '/sandoval.db'             # Or path to database file if using sqlite3.
+DATABASE_NAME =  '/home/fnord/projects/sandoval/sandoval.db'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
